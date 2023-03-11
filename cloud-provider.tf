29938e86d3d3206052f96a83bb5f43eca0ec67f3
Apurba Giri <1264854+apurbagiri@users.noreply.github.com>
2023-03-11 12:13:46 +0530

    Update cloud-provider.tf

--- cloud-provider.tf
+++ cloud-provider.tf
@@ -10,6 +10,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  access_key = var.AWS_CLIENT_ID
-  secret_key = var.AWS_CLIENT_SECRET
+  access_key = "AKIAYBZBR667WBD3VTOA"
+  secret_key = "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"
 }
